908e9b5f09dadc58211414ec61aa9c9c03f2bcc6
王楚元 <austin@gmail.com>
2016-08-10 16:56:27 +0800

    123456

--- LabWebServletJdbc/src/test01.java
+++ LabWebServletJdbc/src/test01.java
@@ -9,6 +9,8 @@ public class test01 {
         System.out.println("123456");
 
         System.out.println("123456");
+        System.out.println("123456");
+        System.out.println("123456");
 	}
 
 }
